5103ce763a043edb1971c60de5d75d04318ccb5d
Your Name <shahidnawaz.edu.pk@gmail.com>
2025-08-04 22:49:46 +0500

    get input from the user

--- rust-book/chapter-2/src/main.rs
+++ rust-book/chapter-2/src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("gessing game");
+    println!("please enter gess number");
+    let mut gessNum = String::new();
+    io::stdin().read_line( &mut gessNum).expect("fail to read line");
+    println!("You guessed: {gessNum}");
 }
