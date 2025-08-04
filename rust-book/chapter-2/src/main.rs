6c88286d1c9786c96527cb3abc2df4376c09d5a5
Your Name <shahidnawaz.edu.pk@gmail.com>
2025-08-04 22:58:51 +0500

    generate randam number

--- rust-book/chapter-2/src/main.rs
+++ rust-book/chapter-2/src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
+use rand::Rng;
 fn main() {
     println!("gessing game");
+    let secret_num = rand::thread_rang().gen_range(1..=100);
+    println!("The secret number is: {secret_num}");
     println!("please enter gess number");
     let mut gessNum = String::new();
     io::stdin().read_line( &mut gessNum).expect("fail to read line");
