b8267377c787221dbb1d8104be761986ef85b47a
Your Name <shahidnawaz.edu.pk@gmail.com>
2025-08-04 22:37:04 +0500

    start chapter 2

---
+++ rust-book/chapter-2/target/debug/chapter-2.d
@@ -0,0 +1,1 @@
+/Users/app/Documents/solana-school/Advance-Rust/rust-book/chapter-2/target/debug/chapter-2: /Users/app/Documents/solana-school/Advance-Rust/rust-book/chapter-2/src/main.rs
---
+++ rust-book/chapter-2/target/debug/deps/chapter_2-a9d20152976d413a.d
@@ -0,0 +1,5 @@
+/Users/app/Documents/solana-school/Advance-Rust/rust-book/chapter-2/target/debug/deps/chapter_2-a9d20152976d413a.d: src/main.rs
+
+/Users/app/Documents/solana-school/Advance-Rust/rust-book/chapter-2/target/debug/deps/chapter_2-a9d20152976d413a: src/main.rs
+
+src/main.rs:
