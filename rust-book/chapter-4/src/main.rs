8baeb4dd128e7dc19f9b5306838b694b383c3a27
Your Name <shahidnawaz.edu.pk@gmail.com>
2025-08-06 22:51:55 +0500

    return ownership

--- rust-book/chapter-4/src/main.rs
+++ rust-book/chapter-4/src/main.rs
@@ -2,9 +2,11 @@ fn main() {
     let mut s1 = String::from("Hello");
     s1.push_str(", world");
     getOwnership(s1);
+    let s2 = String::from("Hello");
+    let s3 = returnOwnership(s2);
+    println!("s3:{}", s3);
     let x = 5;
     getCopy(x);
-    println!("{}", x);
 }
 
 fn getOwnership(name:String){
@@ -13,4 +15,8 @@ fn getOwnership(name:String){
 
 fn getCopy(y:i32){
     println!("y:{}",y);
+}
+
+fn returnOwnership(name:String) -> String {
+    name
 }
