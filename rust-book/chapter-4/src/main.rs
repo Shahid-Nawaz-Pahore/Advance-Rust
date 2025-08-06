759f61df29e920bc2a414b8420d2cc6d7462f842
Your Name <shahidnawaz.edu.pk@gmail.com>
2025-08-06 22:45:22 +0500

    string pass the ownership and pass value

--- rust-book/chapter-4/src/main.rs
+++ rust-book/chapter-4/src/main.rs
@@ -1,5 +1,16 @@
 fn main() {
-    let mut x = String::from("Hello");
-    x.push_str(", world");
+    let mut s1 = String::from("Hello");
+    s1.push_str(", world");
+    getOwnership(s1);
+    let x = 5;
+    getCopy(x);
     println!("{}", x);
+}
+
+fn getOwnership(name:String){
+    println!("name:{}",name);
+}
+
+fn getCopy(y:i32){
+    println!("y:{}",y);
 }
