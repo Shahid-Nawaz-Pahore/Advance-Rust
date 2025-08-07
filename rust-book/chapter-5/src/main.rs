9a7ba5112cc3b6a57d82c4383d03064f3a368a55
Your Name <shahidnawaz.edu.pk@gmail.com>
2025-08-07 23:32:05 +0500

    debug and fix

--- rust-book/chapter-5/src/main.rs
+++ rust-book/chapter-5/src/main.rs
@@ -4,7 +4,14 @@ struct User {
     email:String,
     count:i32,
 }
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+struct AlwaysEqual;
 fn main() {
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+    let subject = AlwaysEqual;
+
     let user1 = User {
       status:true,
       name:String::from("Shahid Nawaz"),
@@ -16,17 +23,18 @@ fn main() {
     println!("User Email: {}", user1.email);
     println!("User Count: {}", user1.count);
     let user2 = User {
-        active: user1.active,
-        username: user1.username,
+        status: user1.status,
+        name: user1.name,
         email: String::from("another@example.com"),
-        sign_in_count: user1.sign_in_count,
+        count: user1.count,
     };
+
 }
-fn build_user(email: String, username: String) -> User {
+fn build_user(email: String, name: String) -> User {
     User {
-        active: true,
-        username,
+        status: true,
+        name,
         email,
-        sign_in_count: 1,
+        count: 1,
     }
 }
