96e737894fdab6da69f8cb8cba4bd62b957d8cd0
Your Name <shahidnawaz.edu.pk@gmail.com>
2025-08-08 23:38:49 +0500

    m direction event

--- rust-book/chapter-6/src/main.rs
+++ rust-book/chapter-6/src/main.rs
@@ -1,3 +1,17 @@
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let dir = Direction::North;
+
+    match dir {
+        Direction::North => println!("Going up!"),
+        Direction::South => println!("Heading down!"),
+        Direction::East => println!("Moving right!"),
+        Direction::West => println!("Moving left!"),
+    }
 }
